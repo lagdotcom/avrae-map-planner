d7f8ed6d0883f170650d959be5ad5be9d15b95f4
Paul Davies <lagdotcom@gmail.com>
2021-04-19 14:56:30 +0100

    added visuals for each door type

--- src/MapView.tsx
+++ src/MapView.tsx
@@ -6,7 +6,7 @@ import BattlePlan, {
   Unit,
   Wall,
 } from "./BattlePlan";
-import { columnLabel, en } from "./tools";
+import { columnLabel, en, lerp } from "./tools";
 
 const sizeLookup: { [size: string]: number } = {
   T: 0.3,
@@ -77,6 +77,22 @@ function MapUnit({
   );
 }
 
+function getMiddle(wall: Wall, diff = 0.5) {
+  const dx = wall.sx - wall.ex;
+  const dy = wall.sy - wall.ey;
+  const len = Math.sqrt(dx * dx + dy * dy);
+  const p1 = (len - diff) / 2;
+  const p2 = (len + diff) / 2;
+  const x1 = lerp(wall.sx, wall.ex, p1 / len);
+  const y1 = lerp(wall.sy, wall.ey, p1 / len);
+  const x2 = lerp(wall.sx, wall.ex, p2 / len);
+  const y2 = lerp(wall.sy, wall.ey, p2 / len);
+  const xm = lerp(wall.sx, wall.ex, 0.5);
+  const ym = lerp(wall.sy, wall.ey, 0.5);
+
+  return { x1, y1, x2, y2, xm, ym };
+}
+
 function MapWall({ plan, wall }: { plan: BattlePlan; wall: Wall }) {
   const size = plan.gridsize || 40;
 
@@ -90,6 +106,149 @@ function MapWall({ plan, wall }: { plan: BattlePlan; wall: Wall }) {
       strokeWidth={2}
     />
   );
+}
+
+function MapOpenDoor({ plan, wall }: { plan: BattlePlan; wall: Wall }) {
+  const size = plan.gridsize || 40;
+  const { x1, y1, x2, y2 } = getMiddle(wall);
+
+  return (
+    <>
+      <line
+        x1={wall.sx * size}
+        y1={wall.sy * size}
+        x2={wall.ex * size}
+        y2={wall.ey * size}
+        stroke={ColourValues[wall.colour || "k"]}
+        strokeWidth={2}
+      />
+      <line
+        x1={x1 * size}
+        y1={y1 * size}
+        x2={x2 * size}
+        y2={y2 * size}
+        stroke="white"
+        strokeWidth={3}
+      />
+    </>
+  );
+}
+
+function MapClosedDoor({ plan, wall }: { plan: BattlePlan; wall: Wall }) {
+  const size = plan.gridsize || 40;
+  const stroke = ColourValues[wall.colour || "k"];
+  const { x1, y1, x2, y2 } = getMiddle(wall);
+
+  return (
+    <>
+      <line
+        x1={wall.sx * size}
+        y1={wall.sy * size}
+        x2={wall.ex * size}
+        y2={wall.ey * size}
+        stroke={stroke}
+        strokeWidth={2}
+      />
+      <line
+        x1={x1 * size}
+        y1={y1 * size}
+        x2={x2 * size}
+        y2={y2 * size}
+        stroke={stroke}
+        strokeWidth={6}
+      />
+      <line
+        x1={x1 * size}
+        y1={y1 * size}
+        x2={x2 * size}
+        y2={y2 * size}
+        stroke="white"
+        strokeWidth={4}
+      />
+    </>
+  );
+}
+
+function MapDoubleDoor({ plan, wall }: { plan: BattlePlan; wall: Wall }) {
+  const size = plan.gridsize || 40;
+  const stroke = ColourValues[wall.colour || "k"];
+  const { x1, y1, x2, y2 } = getMiddle(wall, 0.6);
+  const { x1: x3, y1: y3, x2: x4, y2: y4 } = getMiddle(wall, 0.05);
+
+  return (
+    <>
+      <line
+        x1={wall.sx * size}
+        y1={wall.sy * size}
+        x2={wall.ex * size}
+        y2={wall.ey * size}
+        stroke={stroke}
+        strokeWidth={2}
+      />
+      <line
+        x1={x1 * size}
+        y1={y1 * size}
+        x2={x2 * size}
+        y2={y2 * size}
+        stroke={stroke}
+        strokeWidth={6}
+      />
+      <line
+        x1={x1 * size}
+        y1={y1 * size}
+        x2={x3 * size}
+        y2={y3 * size}
+        stroke="white"
+        strokeWidth={4}
+      />
+      <line
+        x1={x2 * size}
+        y1={y2 * size}
+        x2={x4 * size}
+        y2={y4 * size}
+        stroke="white"
+        strokeWidth={4}
+      />
+    </>
+  );
+}
+
+function MapSecretDoor({ plan, wall }: { plan: BattlePlan; wall: Wall }) {
+  const size = plan.gridsize || 40;
+  const stroke = ColourValues[wall.colour || "k"];
+  const { xm, ym } = getMiddle(wall);
+
+  return (
+    <>
+      <MapOpenDoor plan={plan} wall={wall} />
+      <text
+        x={xm * size}
+        y={ym * size}
+        textAnchor="middle"
+        dominantBaseline="central"
+        stroke={stroke}
+        strokeWidth={2}
+        pointerEvents="none"
+      >
+        S
+      </text>
+    </>
+  );
+}
+
+function MapLine({ plan, wall }: { plan: BattlePlan; wall: Wall }) {
+  switch (wall.door) {
+    case "o":
+      return <MapOpenDoor plan={plan} wall={wall} />;
+    case "d":
+      return <MapClosedDoor plan={plan} wall={wall} />;
+    case "s":
+      return <MapSecretDoor plan={plan} wall={wall} />;
+    case "b":
+      return <MapDoubleDoor plan={plan} wall={wall} />;
+    default:
+      return <MapWall plan={plan} wall={wall} />;
+  }
 }
 
 export function MapView({
@@ -164,7 +323,7 @@ export function MapView({
         pointerEvents="none"
       />
       {plan.walls.map((w, i) => (
-        <MapWall key={"w" + i} plan={plan} wall={w} />
+        <MapLine key={"w" + i} plan={plan} wall={w} />
       ))}
       {plan.units.map((u, i) => (
         <MapUnit
